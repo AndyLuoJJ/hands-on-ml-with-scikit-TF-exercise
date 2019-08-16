8dbf702c9da83b474446df06467ad879524b8784
Andy Luo <bigluojack@126.com>
2019-08-16 09:44:10 +0800

    Complete exercise 1 of chapter 3
    The answer has not been proved.

---
+++ chapter_2/configs.py
@@ -0,0 +1,5 @@
+# This file stores all configurations of the project
+
+DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml/master/"
+HOUSING_PATH = "dataset/housing"
+HOUSING_URL = DOWNLOAD_ROOT + HOUSING_PATH + "/housing.tgz"
